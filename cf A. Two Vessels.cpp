1f90875b887ea0cb4eaa5f64d624fee273c2abdd
Nadia147 <77375515+Nadia147@users.noreply.github.com>
2023-09-08 12:29:18 +0600

    updated

---
+++ cf A. Two Vessels.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int f(int n);
+bool s[10000];
+long long int ff[10000];
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        double a,b,c;
+        double d;
+        cin>>a>>b>>c;
+
+        cout<<ceil((abs(a-b))/(2*c))<<endl;
+    }
+}
